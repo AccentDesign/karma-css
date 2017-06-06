8751185084a702c61ef64da9ef6a9d0606ca6dee
Stuart George <stuart.bigmassa@gmail.com>
2017-06-07 00:30:49 +0100

    some css lint fixes and csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var $ = require('gulp-load-plugins')();
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var autoprefixer = require('autoprefixer');
 
 var config = {
     publicDir: 'dist',
     scssDir: 'scss',
     autoprefixer: {
-        "browsers": [
-            "last 2 versions",
-            "IE 9",
-            "Safari 8"
+        browsers: [
+            'Chrome >= 35',
+            'Firefox >= 38',
+            'Edge >= 12',
+            'Explorer >= 10',
+            'iOS >= 8',
+            'Safari >= 8',
+            'Android 2.3',
+            'Android >= 4',
+            'Opera >= 12'
         ],
         "cascade": false
     },
@@ -17,21 +24,24 @@ var config = {
         "includePaths": [
             'node_modules'
         ]
-    }
+    },
+    csso: {}
 };
 
 gulp.task('scss', function() {
     return gulp.src(config.scssDir + '/karma.scss')
     .pipe($.sourcemaps.init())
     .pipe($.sass(config.sass))
-    .pipe($.autoprefixer(config.autoprefixer))
+    .pipe($.postcss([
+        autoprefixer(config.autoprefixer)
+    ]))
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(config.publicDir));
 });
 
 gulp.task('min', function () {
   return gulp.src(config.publicDir + '/karma.css')
-    .pipe($.cssmin())
+    .pipe($.csso(config.csso))
     .pipe($.rename({suffix: '.min'}))
     .pipe(gulp.dest(config.publicDir))
     .pipe($.livereload());
