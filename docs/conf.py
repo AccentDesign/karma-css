b4cfcf617289bb75334186332e763e91d52d8355
Stuart George <stuart.bigmassa@gmail.com>
2018-02-01 21:48:09 +0000

    changed title

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'KarmaCSS'
+project = 'Karma CSS'
 copyright = '2018, Stuart George'
 author = 'Stuart George'
 
