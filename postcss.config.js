68595aa785fa45449b3d33adb6d997e28507098b
Stuart George <stuart.bigmassa@gmail.com>
2018-02-02 23:02:23 +0000

    refactor to no longer require gulpfile

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = {
+    map: {
+        inline: false,
+        annotation: true,
+        sourcesContent: true
+    },
+    plugins: {
+        'postcss-discard-comments': {},
+        autoprefixer: {
+            cascade: false
+        },
+    }
+}
